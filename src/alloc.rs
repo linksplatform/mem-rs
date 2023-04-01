5aba3c3ea2182ff2d7bcd57db87c939daf987146
Vladimir <vova.krivosheya.2004@gmail.com>
2023-04-02 00:43:24 +0300

    completed fn grow

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,13 +1,14 @@
 use crate::{Error::CapacityOverflow, RawMem, Result};
 use std::{
-    alloc::{Allocator, Layout},
-    cmp::Ordering,
+    alloc::{Allocator, Layout, System},
     marker::PhantomData,
     mem,
     mem::MaybeUninit,
-    ptr::{drop_in_place, NonNull},
+    ptr,
+    ptr::NonNull,
+    slice,
 };
-use tap::Pipe;
+//use tap::Pipe;
 
 pub struct Alloc<T, A: Allocator> {
     ptr: NonNull<T>,
@@ -16,7 +17,7 @@ pub struct Alloc<T, A: Allocator> {
     _marker: PhantomData<T>,
 }
 
-impl<T: Default, A: Allocator> Alloc<T, A> {
+impl<T, A: Allocator> Alloc<T, A> {
     pub const fn new(alloc: A) -> Self {
         Self {
             ptr: NonNull::dangling(),
@@ -45,9 +46,6 @@ impl<T: Default, A: Allocator> Alloc<T, A> {
 
 impl<T, A: Allocator> RawMem for Alloc<T, A> {
     type Item = T;
-    fn allocated(&mut self) -> &mut [Self::Item] {
-        todo!()
-    }
 
     unsafe fn grow(
         &mut self,
@@ -57,15 +55,17 @@ impl<T, A: Allocator> RawMem for Alloc<T, A> {
         let req_cap = self.len.checked_add(cap).ok_or(CapacityOverflow)?;
         let new_layout = Layout::array::<T>(req_cap)?;
         let ptr = if let Some((ptr, old_layout)) = self.current_memory() {
-            self.alloc.grow(ptr, old_layout, new_layout)
+            self.alloc.grow(ptr, old_layout, new_layout)?
         } else {
-            self.alloc.allocate(new_layout)
+            self.alloc.allocate(new_layout)?
         };
-        fill(
-            NonNull::slice_from_raw_parts(ptr?.cast::<T>(), req_cap)
-                .get_unchecked_mut(self.len..)
-                .as_uninit_slice_mut(),
-        );
+        let uninit = NonNull::slice_from_raw_parts(ptr.cast::<T>(), req_cap)
+            .get_unchecked_mut(self.len..)
+            .as_uninit_slice_mut();
+        fill(uninit);
+        self.ptr = ptr.cast::<T>();
+        self.len = req_cap;
+        Ok(MaybeUninit::slice_assume_init_mut(uninit))
     }
 
     fn shrink(&mut self, cap: usize) -> Result<()> {
@@ -73,23 +73,7 @@ impl<T, A: Allocator> RawMem for Alloc<T, A> {
     }
 }
 
-// impl<T, A: Allocator> Drop for Alloc<T, A> {
-//     fn drop(&mut self) {
-//         // SAFETY: ptr is valid slice
-//         // SAFETY: items is friendly to drop
-//         unsafe { self.base.ptr.as_mut().pipe(|slice| drop_in_place(slice)) }
-//
-//         let _: Result<_> = try {
-//             let ptr = self.base.ptr;
-//             let layout = Layout::array::<T>(ptr.len())?;
-//             // SAFETY: ptr is valid slice
-//             unsafe {
-//                 let ptr = ptr.as_non_null_ptr().cast();
-//                 self.alloc.deallocate(ptr, layout);
-//             }
-//         };
-//     }
-// }
+impl<T, A: Allocator> Drop for Alloc<T, A> {}
 
 unsafe impl<T: Sync, A: Allocator + Sync> Sync for Alloc<T, A> {}
 unsafe impl<T: Send, A: Allocator + Send> Send for Alloc<T, A> {}
