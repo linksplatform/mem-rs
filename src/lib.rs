ab4175fe67be6734da9fbf8444baf86753f755db
Vladimir <vova.krivosheya.2004@gmail.com>
2023-04-09 20:10:05 +0300

    add test to lib

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,10 @@ pub use {
 
 mod alloc;
 mod file_mapped;
+mod global;
 mod raw_mem;
 mod raw_place;
+mod temp_file;
 mod utils;
 
 fn _assertion() {
@@ -28,4 +30,19 @@ fn _assertion() {
 
     assert_sync_send::<FileMapped<()>>();
     assert_sync_send::<Alloc<(), std::alloc::Global>>();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    pub fn inner<M: RawMem>(mut mem: M, val: M::Item) -> Result<()>
+    where
+        M::Item: Clone,
+    {
+        mem.grow_filled(10, val)?;
+        assert_eq!(mem.allocated().len(), 10);
+        mem.shrink(10)?;
+        assert_eq!(mem.allocated().len(), 0);
+        Ok(())
+    }
 }
