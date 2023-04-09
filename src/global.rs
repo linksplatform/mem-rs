715913a5ebcec101307899d0100a09d7cdf1bbe3
Vladimir <vova.krivosheya.2004@gmail.com>
2023-04-09 19:39:05 +0300

    update global

--- src/global.rs
+++ src/global.rs
@@ -1,82 +1,60 @@
-use crate::{internal, Base, RawMem, Result};
-use std::{
-    alloc::{self, Layout},
-    mem::size_of,
-    ptr::{self, NonNull},
+use {
+    crate::{Alloc, RawMem, RawPlace, Result},
+    std::{
+        alloc::{self},
+        mem::MaybeUninit,
+    },
 };
-use tap::Pipe;
 
-pub struct Global<T>(Base<T>);
+pub struct Global<T>(Alloc<T, alloc::Global>);
 
 impl<T> Global<T> {
-    #[must_use]
     pub const fn new() -> Self {
-        Self(Base::dangling())
-    }
-
-    fn layout_impl(capacity: usize) -> Result<Layout> {
-        Layout::array::<T>(capacity).map_err(Into::into)
+        Self(Alloc::new(alloc::Global))
     }
 }
 
-impl<T: Default> Global<T> {
-    unsafe fn on_reserved_impl(&mut self, new_capacity: usize) -> Result<&mut [T]> {
-        let old_capacity = self.0.allocated();
-        let new_in_bytes = new_capacity * size_of::<T>();
-        let ptr = if self.0.ptr.as_non_null_ptr() == NonNull::dangling() {
-            Self::layout_impl(new_capacity)?.pipe(|layout| alloc::alloc(layout))
-        } else {
-            if new_capacity < old_capacity {
-                self.0.handle_narrow(new_capacity);
-            }
-            let ptr = internal::to_bytes(self.0.ptr).as_mut_ptr();
-            Self::layout_impl(old_capacity)?
-                .pipe(|layout| alloc::realloc(ptr, layout, new_in_bytes))
-        }
-        .pipe(|ptr| NonNull::new_unchecked(ptr))
-        .pipe(|ptr| NonNull::slice_from_raw_parts(ptr, new_in_bytes));
+impl<T> RawMem for Global<T> {
+    type Item = T;
 
-        self.0.ptr = internal::guaranteed_from_bytes(ptr);
-        self.0.handle_expand(old_capacity);
-        self.0.ptr.as_mut().pipe(Ok)
-    }
-}
-
-impl<T: Default> const Default for Global<T> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<T: Default> RawMem<T> for Global<T> {
-    fn alloc(&mut self, capacity: usize) -> Result<&mut [T]> {
-        unsafe { self.on_reserved_impl(capacity) }
-    }
-
-    fn allocated(&self) -> usize {
+    fn allocated(&self) -> &[Self::Item] {
         self.0.allocated()
     }
-}
 
-impl<T> Drop for Global<T> {
-    fn drop(&mut self) {
-        // SAFETY: ptr is valid slice
-        // items is friendly to drop
-        unsafe { self.0.ptr.as_mut().pipe(|slice| ptr::drop_in_place(slice)) }
+    fn allocated_mut(&mut self) -> &mut [Self::Item] {
+        self.0.allocated_mut()
+    }
 
-        let _: Result<_> = try {
-            let ptr = self.0.ptr;
-            let layout = Self::layout_impl(ptr.len())?;
-            // SAFETY: ptr is valid slice
-            unsafe {
-                ptr.as_non_null_ptr()
-                    .cast::<u8>()
-                    .as_ptr()
-                    .pipe(|ptr| alloc::dealloc(ptr, layout));
-            }
-        };
+    unsafe fn grow(
+        &mut self,
+        cap: usize,
+        fill: impl FnOnce(&mut [MaybeUninit<Self::Item>]),
+    ) -> Result<&mut [Self::Item]> {
+        self.0.grow(cap, fill)
+    }
+
+    fn shrink(&mut self, cap: usize) -> Result<()> {
+        self.0.shrink(cap)
     }
 }
 
-unsafe impl<T: Sync> Sync for Global<T> {}
-unsafe impl<T: Send> Send for Global<T> {}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    fn inner<M: RawMem>(mut mem: M, val: M::Item) -> Result<()>
+    where
+        M::Item: Clone,
+    {
+        mem.grow_filled(10, val)?;
+        assert_eq!(mem.allocated().len(), 10);
+        mem.shrink(10)?;
+        assert_eq!(mem.allocated().len(), 0);
+        Ok(())
+    }
+
+    #[test]
+    fn for_globalg_test() -> Result<()> {
+        inner(Global::new(), "lol".to_string())?;
+        Ok(())
+    }
+}
