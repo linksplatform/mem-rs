aff8c0ea714b72f57df0bf926de37cf7cf09bd5b
flakeed <flakeeed@gmail.com>
2023-09-06 12:06:51 +0300

    Rename alias 'ResultBoxError' to 'Result'

--- tests/miri.rs
+++ tests/miri.rs
@@ -1,8 +1,8 @@
 use {platform_mem::RawMem, std::error::Error};
 
-type ResultBoxError = Result<(), Box<dyn Error>>;
+type Result = std::result::Result<(), Box<dyn Error>>;
 
-pub fn miri(mut mem: impl RawMem<Item = String>) -> ResultBoxError {
+pub fn miri(mut mem: impl RawMem<Item = String>) -> Result {
     const GROW: usize = if cfg!(miri) { 100 } else { 10_000 };
 
     let val = String::from("foo");
